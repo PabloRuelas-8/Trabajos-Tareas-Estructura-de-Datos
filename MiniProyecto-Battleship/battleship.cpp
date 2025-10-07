8ee581526d1898d1b0d22f8ea0bfd245f0cf3f7b
PabloRuelas-8 <ruelaslopezjuanpablo@gmail.com>
2025-10-06 20:42:48 -0700

    Battleship finalizado

--- MiniProyecto-Battleship/battleship.cpp
+++ MiniProyecto-Battleship/battleship.cpp
@@ -4,21 +4,21 @@
 #include <cctype>
 using namespace std;
 
-const int size_casillas = 10;
+const int TAM = 10;
 
-void inicializar(char tablero[size_casillas][size_casillas]) {
-    for (int i = 0; i < size_casillas; i++)
-        for (int j = 0; j < size_casillas; j++)
+void inicializar(char tablero[TAM][TAM]) {
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
             tablero[i][j] = '.';
 }
 
-void imprimir(char tablero[size_casillas][size_casillas], bool ocultar) {
+void imprimir(char tablero[TAM][TAM], bool ocultar) {
     cout << "   ";
-    for (int j = 0; j < size_casillas; j++) cout << char('A' + j) << " ";
+    for (int j = 0; j < TAM; j++) cout << char('A' + j) << " ";
     cout << "\n";
-    for (int i = 0; i < size_casillas; i++) {
+    for (int i = 0; i < TAM; i++) {
         cout << setw(2) << i + 1 << " ";
-        for (int j = 0; j < size_casillas; j++) {
+        for (int j = 0; j < TAM; j++) {
             char c = tablero[i][j];
             if (ocultar && c != '.' && c != 'X' && c != '-') cout << ". ";
             else cout << c << " ";
@@ -27,19 +27,19 @@ void imprimir(char tablero[size_casillas][size_casillas], bool ocultar) {
     }
 }
 
-bool convertir(string s, int &fila, int &col) { 
+bool convertir(string s, int &fila, int &col) {
     if (s.size() < 2) return false;
     col = toupper(s[0]) - 'A';
     fila = stoi(s.substr(1)) - 1;
-    if (fila < 0 || fila >= size_casillas || col < 0 || col >= size_casillas) return false;
+    if (fila < 0 || fila >= TAM || col < 0 || col >= TAM) return false;
     return true;
 }
 
-void colocarBarco(char tablero[size_casillas][size_casillas], int tamBarco, char simbolo) {
+void colocarBarco(char tablero[TAM][TAM], int tamBarco, char simbolo) {
     bool puesto = false;
     while (!puesto) {
         imprimir(tablero, false);
-        cout << "Coloca barco de " << tamBarco << " casillas " << "(escribir asi, A1 H): ";
+        cout << "Coloca barco de " << tamBarco << " casillas (ej A1 H): ";
         string coord; char ori;
         cin >> coord >> ori;
         int fila, col;
@@ -47,14 +47,14 @@ void colocarBarco(char tablero[size_casillas][size_casillas], int tamBarco, char
         ori = toupper(ori);
         bool valido = true;
         if (ori == 'H') {
-            if (col + tamBarco > size_casillas) { valido = false; }
+            if (col + tamBarco > TAM) { valido = false; }
             else {
                 for (int j = 0; j < tamBarco; j++)
                     if (tablero[fila][col + j] != '.') valido = false;
                 if (valido) for (int j = 0; j < tamBarco; j++) tablero[fila][col + j] = simbolo;
             }
         } else if (ori == 'V') {
-            if (fila + tamBarco > size_casillas) { valido = false; }
+            if (fila + tamBarco > TAM) { valido = false; }
             else {
                 for (int i = 0; i < tamBarco; i++)
                     if (tablero[fila + i][col] != '.') valido = false;
@@ -67,11 +67,30 @@ void colocarBarco(char tablero[size_casillas][size_casillas], int tamBarco, char
     }
 }
 
+bool disparar(char tablero[TAM][TAM]) {
+    cout << "Dispara (ej B7): ";
+    string coord; cin >> coord;
+    int fila, col;
+    if (!convertir(coord, fila, col)) { cout << "Coordenada invalida\n"; return false; }
+    if (tablero[fila][col] == '.') { tablero[fila][col] = '-'; cout << "Agua!\n"; }
+    else if (tablero[fila][col] == '-' || tablero[fila][col] == 'X') { cout << "Ya disparaste ahi\n"; }
+    else { tablero[fila][col] = 'X'; cout << "Tocado!\n"; }
+    return true;
+}
+
+bool quedanBarcos(char tablero[TAM][TAM]) {
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
+            if (tablero[i][j] != '.' && tablero[i][j] != '-' && tablero[i][j] != 'X')
+                return true;
+    return false;
+}
 
 int main() {
-    char t1[size_casillas][size_casillas], t2[size_casillas][size_casillas];
+    char t1[TAM][TAM], t2[TAM][TAM];
     inicializar(t1); inicializar(t2);
 
+    // Colocacion de barcos
     cout << "Jugador 1 coloca sus barcos\n";
     colocarBarco(t1, 5, 'P'); // portaaviones
     colocarBarco(t1, 4, 'A'); // acorazado
@@ -79,7 +98,7 @@ int main() {
     colocarBarco(t1, 3, 'S'); // submarino
     colocarBarco(t1, 2, 'T'); // patrullero
 
-    system("cls"); 
+    system("cls");
     cout << "Jugador 2 coloca sus barcos\n";
     colocarBarco(t2, 5, 'P');
     colocarBarco(t2, 4, 'A');
@@ -87,4 +106,18 @@ int main() {
     colocarBarco(t2, 3, 'S');
     colocarBarco(t2, 2, 'T');
 
+    int turno = 1;
+    while (quedanBarcos(t1) && quedanBarcos(t2)) {
+        cout << "\n=== Turno Jugador " << turno << " ===\n";
+        if (turno == 1) { imprimir(t2, true); disparar(t2); turno = 2; }
+        else { imprimir(t1, true); disparar(t1); turno = 1; }
+    }
+    if (quedanBarcos(t1)){
+        cout << "Jugador 1 gana!\n";
+        system("pause");
+    }
+    else{
+        cout << "Jugador 2 gana!\n";
+        system("pause");
+    } 
 }
